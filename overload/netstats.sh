69d38c56c29581f715b4d73443e3015a3ab0816c
Seth Jadolo Odhiambo.O <sethodhiambojad@gmail.com>
2025-10-10 22:15:01 +0300

    ui and usage tweaks

--- overload/netstats.sh
+++ overload/netstats.sh
@@ -5,7 +5,6 @@
 TARGET=1.1.1.1
 TMPDIR="$HOME/.cache/netstats"
 TMPFILE="$TMPDIR/netstats.json"
-LAST_SPEEDTEST_FILE="$TMPDIR/.speedtest_last_run"
 LOGFILE="/tmp/netstats.log"
 
 mkdir -p "$TMPDIR"
@@ -30,37 +29,14 @@ update_stats() {
     # ISP info
     ISP=$(/usr/bin/curl -s https://ipinfo.io/json | /usr/bin/jq -r '.org // "Unknown ISP"')
 
-    # Speedtest every 30 minutes
-    NOW=$(date +%s)
-    if [[ ! -f $LAST_SPEEDTEST_FILE ]] || (( NOW - $(cat $LAST_SPEEDTEST_FILE) > 1800 )); then
-        echo $NOW > "$LAST_SPEEDTEST_FILE"
-        SPEED_OUTPUT=$(/usr/bin/speedtest-cli --json 2>/dev/null)
-        if [[ $? -eq 0 && -n "$SPEED_OUTPUT" ]]; then
-            echo "$SPEED_OUTPUT" | jq '{download: .download / 1e6, upload: .upload / 1e6}' > "${TMPFILE}.tmp"
-            mv "${TMPFILE}.tmp" "$TMPFILE"
-            DOWNLOAD=$(jq -r '.download' "$TMPFILE")
-            UPLOAD=$(jq -r '.upload' "$TMPFILE")
-        else
-            log "Speedtest failed at $(date)"
-            DOWNLOAD=null
-            UPLOAD=null
-        fi
-    else
-        DOWNLOAD=null
-        UPLOAD=null
-    fi
-
-    # Write full stats JSON
+    # Write stats JSON
     /usr/bin/jq -n \
         --arg isp "$ISP" \
         --argjson latency "${AVG_LAT:-0}" \
         --argjson jitter "${JITTER:-0}" \
         --argjson loss "${LOSS:-0}" \
-        --argjson download "${DOWNLOAD:-0}" \
-        --argjson upload "${UPLOAD:-0}" \
-        '{isp:$isp, latency:$latency, jitter:$jitter, loss:$loss, download:$download, upload:$upload}' \
+        '{isp:$isp, latency:$latency, jitter:$jitter, loss:$loss}' \
         > "$TMPFILE"
-
     log "Stats updated: $(cat "$TMPFILE")"
 }
 
